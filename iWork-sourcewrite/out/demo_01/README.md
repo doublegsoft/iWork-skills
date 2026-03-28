31b94fdd7f8c21852c6aae1a86c486da7f60fde0
ganguo <ganguo>
2026-03-28 09:36:45 +0800

    initial project

--- iWork-sourcewrite/out/demo_01/README.md
+++
@@ -1,8 +0,0 @@
-# Example Project
-
-This is a demo project showing how to structure files.
-
-## Run
-
-```bash
-mvn spring-boot:run
--- iWork-sourcewrite/out/src/main/java/com/example/service/FileService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-public class FileService {
-
-    public void writeFile(String path, String content) {
-        System.out.println("Writing to file: " + path);
-        // TODO: implement write logic
-    }
-}
